85bc96b4a81507c2e7feec2b963e3621fc65224a
Aman Srivastava <amansrivastava_29@yahoo.com>
2017-12-21 05:37:07 -0700

    Linked DB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/sna4slack',
+    database: 'mongodb://aman2909:aman2909@ds161146.mlab.com:61146/sna4slack',
     secret: 'yoursecret'
 };
