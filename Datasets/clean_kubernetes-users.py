53816afeb405eeede7a0ab37da569cc57c0face9
Sanchit Narang <snarang2@asu.edu>
2018-02-15 12:02:38 -0700

    Task #118 Split Date and Time to gain metrics

--- Datasets/clean_kubernetes-users.py
+++ Datasets/clean_kubernetes-users.py
@@ -12,4 +12,12 @@ df1 = df1.drop(['if'], axis=1)
 df1['Sender'].replace('', np.nan, inplace=True)
 df1.dropna(subset=['Sender'], inplace=True)
 
+#Convert time column to string
+df1[['Time']].astype(str)
+
+#Split date and time
+df1['Date'] = df1.Time.str[:12]
+df1['Time'] = df1.Time.str[13:18]
+
+
 df1.to_csv('/Users/sanchitnarang94/Desktop/SER 517/SNA4Slack-master/Datasets/clean_kubernetes-users.csv')
