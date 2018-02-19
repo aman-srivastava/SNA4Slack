0684ef40caaad4809b4fc81be5e22be76841f948
Sanchit Narang <snarang2@asu.edu>
2018-02-19 11:07:35 -0700

    Task #119: Update getMetrics for top 3 users

--- Datasets/getMetrics.py
+++ Datasets/getMetrics.py
@@ -13,3 +13,26 @@ df1['Sender'].value_counts().max()
 
 #User with max messages.
 df1['Sender'].value_counts().argmax()
+
+#User with highest message count
+df1['Sender'].value_counts().index[0]
+#Highest message count
+df1['Sender'].value_counts()[0]
+
+
+#User with 3rd highest message count
+df1['Sender'].value_counts().index[2]
+#3rd highest message count
+df1['Sender'].value_counts()[2]
+
+
+#User with 2nd highest message count
+df1['Sender'].value_counts().index[1]
+#2nd highest message count
+df1['Sender'].value_counts()[1]
+
+
+#User with 3rd highest message count
+df1['Sender'].value_counts().index[2]
+#3rd highest message count
+df1['Sender'].value_counts()[2]
