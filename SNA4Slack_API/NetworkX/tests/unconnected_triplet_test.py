c9037e952059a0763ff674a101bba7ac974a480a
nikhil108 <nikhiltibrewal108@gmail.com>
2018-02-21 23:42:47 -0700

    Task#94 addition of test for average ckustering in unconnected triplet graph

--- SNA4Slack_API/NetworkX/tests/unconnected_triplet_test.py
+++ SNA4Slack_API/NetworkX/tests/unconnected_triplet_test.py
@@ -21,18 +21,27 @@ class TestClass(object):
         for node in TEST_DATA["degree_centrality"].keys():
             assert self.graph_builder.graph.nodes[node]["degree_centrality"] == \
                    TEST_DATA["degree_centrality"][node]
+	
+	def compute_avg_clustering(self):
+        ac = nx.average_clustering(self.graph)
+        self.graph.graph[AVERAGE_CLUSTERING] = ac
+        logging.debug(self.__class__.__name__ + ": Clustering computed.")
 
     def test_betweeness_centrality(self):
         self.graph_builder.compute_betweenness_centrality()
         for node in TEST_DATA["betweenness_centrality"].keys():
-            assert self.graph_builder.graph.nodes[node][
-                       "betweenness_centrality"] == \
+            assert self.graph_builder.graph.nodes[node]["betweenness_centrality"] == \
                    TEST_DATA["betweenness_centrality"][node]
     
     def test_for_density(self):
         self.graph_builder.compute_density()
          assert round(self.graph_builder.graph.graph["density"], 5) == \
                 round(TEST_DATA["density"], 5)
+	
+	def test_for_average_clustering(self):
+        self.graph_builder.compute_avg_clustering()
+        assert round(self.graph_builder.graph.graph["average_clustering"], 5) == \
+               round(TEST_DATA["average_clustering"], 5)
 
     def test_closeness_centrality(self):
         self.graph_builder.compute_closeness_centrality()
