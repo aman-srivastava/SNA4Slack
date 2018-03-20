b405366ec63d5b6e284b4eeeae02ec7c3bcb9333
Sanchit Narang <snarang2@asu.edu>
2018-03-19 17:13:00 -0700

    Task #166 #167- Dynamic Data through Ajax calls and mLab

--- SNA4Slack/SNA4Slack/assets/js/getChannelMainMetrics.js
+++ SNA4Slack/SNA4Slack/assets/js/getChannelMainMetrics.js
@@ -1,10 +1,7 @@
 var team;
 if(window.location.href.includes("?teamName")){
 		team = window.location.href.substring(window.location.href.indexOf("?")+10);
-		document.getElementById("dashboardPageLink").href = "Dashboard.html?teamName="+team;
-		document.getElementById("TeamsPageLink").href = "Teams.html?teamName="+team;
-		document.getElementById("ChannelsPageLink").href = "ChannelMain.html?teamName="+team;
-		document.getElementById("MembersPageLink").href = "MembersMain.html?teamName="+team;
+
 	}
 
 $( document ).ready(function() {
@@ -12,10 +9,6 @@ $( document ).ready(function() {
 	if(window.location.href.includes("?teamName")){
 					teamName = window.location.href.substring(window.location.href.indexOf("?")+10);
 				}
-	document.getElementById("teamNameHeader").innerHTML = "Dashboard | "+teamName;
-	document.getElementById("teamNameSidebar").innerHTML = teamName;
-	document.getElementById("teamURLTag").innerHTML = teamName+".slackarchive.io";
-	document.getElementById("teamURLLink").href = "http://"+teamName+".slackarchive.io";
 	$.ajax({
 		 url: "https://api.mlab.com/api/1/databases/sna4slack/collections/"+teamName+"?apiKey=dPpfbNvB6jRs-hvv-Veb1uVkXnX06Maa",
 		 type: 'GET',
@@ -25,35 +18,59 @@ $( document ).ready(function() {
 					data = data[j]['dataAnalytics'];
 				};
 			}
-			
-			//console.log(data);
-			var noOfMessages = 0
-			var noOfChannels = []
-			var noOfMembers = []
-			for(var i = 0 ; i<data.length ; i++){
-				noOfMessages += parseInt(data[i].messageCount);
 
-				if (noOfMembers.indexOf(data[i].messageSender) === -1){
-				noOfMembers.push(data[i].messageSender);
-				}
-				if (noOfChannels.indexOf(data[i].channelName) === -1){
-				noOfChannels.push(data[i].channelName);
-				}
+			console.log(data);
 
-			}
-			document.getElementById("noOfConversations").innerHTML = noOfMessages;
-			document.getElementById("noOfUsers").innerHTML = noOfMembers.length;
-			document.getElementById("noOfChannels").innerHTML = noOfChannels.length;
+      var channelsDateMap = {}
 
-			document.getElementById("conversationCount").innerHTML = noOfMessages;
-			document.getElementById("memberCount").innerHTML = noOfMembers.length;
-			document.getElementById("channelCount").innerHTML = noOfChannels.length;
-			},
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-        console.log('error', errorThrown);
+
+      for(var i = 0 ; i<data.firstMessage.length ; i++){
+        //console.log(data[i].firstMessage[i])
+        channelsDateMap[data.firstMessage[i].channelName] = data.firstMessage[i].messageTime
       }
+      for(var i in channelsDateMap) {
+        if (channelsDateMap.hasOwnProperty(i)) {
+          console.log('Channel is: ' + i + ' --- ' +'First Message is on ' + channelsDateMap[i]);
+        }
+      }
+
+      console.log('------------------------------------------')
+
+    var channelsMessagesMap = {}
+
+    for(var i = 0 ; i<data.messageCount_channel.length ; i++){
+      channelsMessagesMap[data.messageCount_channel[i].channelName] = data.messageCount_channel[i].msgCount
+    }
+    for(var i in channelsMessagesMap) {
+      if (channelsMessagesMap.hasOwnProperty(i)) {
+        console.log('Channel is: ' + i + ' --- ' +'Total Messages are ' + channelsMessagesMap[i]);
+      }
+
+    }
+
+    var messageArray = []
+    for (var i = 0; i<data.messageCount_channel.length; i++) {
+      messageArray.push(data.messageCount_channel[i].msgCount)
+    }
+
+    var sumMessages = messageArray.reduce((a, b) => a + b, 0);
+
+    var maxMessages = Math.max.apply(null, messageArray)
+    var minMessages = Math.min.apply(null, messageArray)
+
+    console.log('Message Array: '+messageArray);
+    console.log('Total Messages: '+sumMessages);
+    console.log('Average Messages: '+sumMessages/3)
+    console.log('Max Messages: '+maxMessages)
+    console.log('Least Messages: '+minMessages)
+    console.log('Total channels: ' +data.messageCount_channel.length)
+
+			},
+
 	});
 });
+
+
 $( document ).ready(function() {
 	var teamName;
 	if(window.location.href.includes("?teamName")){
@@ -70,18 +87,19 @@ $( document ).ready(function() {
 					data = data[j]['directed-mention-graph'].links;
 				};
 			}
+			//console.log(data);
 			var noOfMentions = 0;
 			var teamSuperstars = {};
 			for(var i = 0 ; i<data.length ; i++){
 				noOfMentions += parseInt(data[i].weight);
-				//console.log(data[i].target+": "+data[i].weight);
 				if(!(data[i].target in teamSuperstars)){
-					teamSuperstars[data[i].target] = data[i].weight;
+					teamSuperstars[data[i].target] = parseInt(data[i].weight);
 				}
 				else{
-					teamSuperstars[data[i].target]+=teamSuperstars[data[i].target];
+					teamSuperstars[data[i].target]=teamSuperstars[data[i].target]+parseInt(data[i].weight);
 				}
 			}
+			console.log(teamSuperstars);
 			var items = Object.keys(teamSuperstars).map(function(key) {
 				return [key, teamSuperstars[key]];
 			});
@@ -92,53 +110,51 @@ $( document ).ready(function() {
 			var max = parseInt(items[0][1]);
 			var min = parseInt(items[items.length-1][1]);
 			items = items.slice(0, 6);
-			
-			document.getElementById("noOfMentions").innerHTML = noOfMentions;
 
-			
+			//document.getElementById("noOfMentions").innerHTML = noOfMentions;
+
+
 			document.getElementById("superstar1username").innerHTML = items[0][0];
 			document.getElementById("superstar1avatar").src = nodes[nodes.map(function(x) {return x.id; }).indexOf(items[0][0])].senderAvatar;
 			document.getElementById("superstar1mentions").innerHTML = items[0][1];
 			document.getElementById("superstar1popularity").max = max;
 			document.getElementById("superstar1popularity").value = items[0][1];
-			
+
 			document.getElementById("superstar2username").innerHTML = items[1][0];
 			document.getElementById("superstar2avatar").src = nodes[nodes.map(function(x) {return x.id; }).indexOf(items[1][0])].senderAvatar;
 			document.getElementById("superstar2mentions").innerHTML = items[1][1];
 			document.getElementById("superstar2popularity").max = max;
 			document.getElementById("superstar2popularity").value = items[1][1];
-			
+
 			document.getElementById("superstar3username").innerHTML = items[2][0];
 			document.getElementById("superstar3avatar").src = nodes[nodes.map(function(x) {return x.id; }).indexOf(items[2][0])].senderAvatar;
 			document.getElementById("superstar3mentions").innerHTML = items[2][1];
 			document.getElementById("superstar3popularity").max = max;
 			document.getElementById("superstar3popularity").value = items[2][1];
-			
+
 			document.getElementById("superstar4username").innerHTML = items[3][0];
 			document.getElementById("superstar4avatar").src = nodes[nodes.map(function(x) {return x.id; }).indexOf(items[3][0])].senderAvatar;
 			document.getElementById("superstar4mentions").innerHTML = items[3][1];
 			document.getElementById("superstar4popularity").max = max;
 			document.getElementById("superstar4popularity").value = items[3][1];
-			
-			
+
+
 			document.getElementById("superstar5username").innerHTML = items[4][0];
 			document.getElementById("superstar5avatar").src = nodes[nodes.map(function(x) {return x.id; }).indexOf(items[4][0])].senderAvatar;
 			document.getElementById("superstar5mentions").innerHTML = items[4][1];
 			document.getElementById("superstar5popularity").max = max;
 			document.getElementById("superstar5popularity").value = items[4][1];
-			
-			
+
+
 			document.getElementById("superstar6username").innerHTML = items[5][0];
 			document.getElementById("superstar6avatar").src = nodes[nodes.map(function(x) {return x.id; }).indexOf(items[5][0])].senderAvatar;
 			document.getElementById("superstar6mentions").innerHTML = items[5][1];
 			document.getElementById("superstar6popularity").max = max;
 			document.getElementById("superstar6popularity").value = items[5][1];
-			
-			
-			
+
+
+
 			},
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-        console.log('error', errorThrown);
-      }
+
 	});
 });
